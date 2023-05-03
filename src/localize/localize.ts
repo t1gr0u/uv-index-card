548b4f0e39edb731d490710de9f650e1cafba2d6
Rui Melo <viper5000@viper5000.net>
2023-05-03 16:47:28 +0100

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,12 +1,14 @@
 import * as de from './languages/de.json';
 import * as en from './languages/en.json';
 import * as fr from './languages/fr.json';
+import * as pt from './languages/pt.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   de: de,
   en: en,
   fr: fr,
+  pt: pt,
 };
 
 export function localize(string: string, search = '', replace = ''): string {
