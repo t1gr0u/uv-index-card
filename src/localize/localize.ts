b2c48659090f98a9f50f983df3fa41d438594158
vmbajop <vmbajop@gmail.com>
2023-07-03 14:33:07 +0200

    Update localize.ts
    Added spanish language
    Añadido idioma español

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -1,26 +1,26 @@
 import * as cs from "./languages/cs.json";
 import * as de from './languages/de.json';
 import * as en from './languages/en.json';
+import * as es from "./languages/es.json";
 import * as fr from './languages/fr.json';
 import * as hu from './languages/hu.json';
 import * as it from "./languages/it.json";
 import * as nl from './languages/nl.json';
 import * as pt from './languages/pt.json';
 import * as sv from "./languages/sv.json";
-import * as es from "./languages/es.json";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const languages: any = {
   cs: cs,
   de: de,
   en: en,
+  es: es,
   fr: fr,
   hu: hu,
   it: it,
   nl: nl,
   pt: pt,
-  sv: sv,
-  es: es,
+  sv: sv,  
 };
 
 export const CARD_LANGUAGES = [...Object.keys(languages), ''].sort();
